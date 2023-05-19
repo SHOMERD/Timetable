74545aeaef4f4f76d3d10dce6e0aa3811c6d068a
SHOMERD <shokershoker31@gmail.com>
2023-05-19 23:19:18 +0700

    Compiled to APK

---
+++ Timetable/Timetable/Views/BufferPages/Plas1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Timetable.Views.BufferPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Plas1 : ContentPage
+    {
+        public Plas1()
+        {
+            InitializeComponent();
+        }
+    }
+}
